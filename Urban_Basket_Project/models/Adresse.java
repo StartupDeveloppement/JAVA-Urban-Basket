25b20cdb019491333178cfd482ca0efb6e18a011
NicolasBerger <nicolas.berger@ynov.com>
2015-12-02 10:00:34 +0100

    modele Adresse

---
+++ Urban_Basket_Project/models/Adresse.java
@@ -0,0 +1,49 @@
+package models;
+
+public class Adresse {
+
+	private int numero;
+	private String rue;
+	private int codePostal;
+	private String ville;
+	private String pays;
+	
+	public Adresse(int num, String rue, int cP, String ville, String pays){
+		this.numero = num;
+		this.rue = rue;
+		this.codePostal = cP;
+		this.ville = ville;
+		this.pays = pays;
+	}
+	
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public String getRue() {
+		return rue;
+	}
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+	public int getCodePostal() {
+		return codePostal;
+	}
+	public void setCodePostal(int codePostal) {
+		this.codePostal = codePostal;
+	}
+	public String getVille() {
+		return ville;
+	}
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+	public String getPays() {
+		return pays;
+	}
+	public void setPays(String pays) {
+		this.pays = pays;
+	}
+}
