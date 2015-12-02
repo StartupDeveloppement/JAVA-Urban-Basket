445ba8bb66f45fe0838a884771c64aea07effb26
Nicolas <nicolassoliveau@gmail.com>
2015-12-02 16:16:05 +0100

    Modification de la classe Reservation :     DateTime => DateResaDeb     Ajout de DateResaFin     Complétioni du constructeur

--- Urban_Basket_Project/models/Reservation.java
+++ Urban_Basket_Project/models/Reservation.java
@@ -8,13 +8,15 @@ import java.time.LocalDateTime;
  * @author SOLIVEAU Nicolas
  */
 public class Reservation {
-    LocalDateTime dateTime;
+    LocalDateTime dateResa_Deb;
+    LocalDateTime dateResa_Fin;
     UserModel user;
     SalleModel salle;
     int duree;
 
-    public Reservation(LocalDateTime dateTime, UserModel user, SalleModel salle, int duree) {
-        this.dateTime = dateTime;
+    public Reservation(LocalDateTime dateResa_Deb, LocalDateTime dateResa_Fin, UserModel user, SalleModel salle, int duree) {
+        this.dateResa_Deb = dateResa_Deb;
+        this.dateResa_Fin = dateResa_Fin;
         this.user = user;
         this.salle = salle;
         this.duree = duree;
